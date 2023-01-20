b2fad1de6618ac918390fa40cc4984912593a181
Medenga <mendenga.takougnadi@hotmail.com>
2023-01-20 15:44:29 +0100

    d

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import './ca.json';
 import { LineChart, PieChart, CartesianGrid, XAxis,YAxis,Tooltip,Legend,Line,Pie } from 'recharts';
 
 const App = () => {
-  const datas = []
-  const [data, setData]=useState(datas);
+  const dt =[]
+  const [data, setData] = useState(dt);
 
   useEffect(() => {
     fetch('uvs.json')
     .then(function(myJson) {
-      console.log(myJson);
       setData(myJson)
     });
   }, []);
 
   return (
     <div className="App">
-      {data}
       <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
@@ -28,6 +26,11 @@ const App = () => {
         <Line type="monotone" dataKey="pv" stroke="#8884d8" />
         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
       </LineChart>
+
+      <PieChart width={730} height={250}>
+        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+        <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+      </PieChart>
 
      
     </div>
